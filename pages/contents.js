8e1a685ed52b51b57ccb913eaa6cfd546e3e4d05
WinsonLoo <weixiong8489@gmail.com>
2020-09-11 12:34:44 +0800

    able to refetch data now need to design card properly

--- pages/contents.js
+++ pages/contents.js
@@ -7,8 +7,8 @@ export const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-const GET_ID = gql`
-query ($isAdult: Boolean, $page: Int, $perPage: Int, $search: String) {
+export const GET_DATA = gql`
+query ($type: MediaType, $isAdult: Boolean, $page: Int, $perPage: Int, $search: String) {
   Page (, page: $page, perPage: $perPage) {
     pageInfo {
       total
@@ -17,7 +17,7 @@ query ($isAdult: Boolean, $page: Int, $perPage: Int, $search: String) {
       hasNextPage
       perPage
     }
-    media (isAdult: $isAdult, search: $search) {
+    media (type: $type, isAdult: $isAdult, search: $search) {
       id
       title {
         romaji
@@ -34,11 +34,51 @@ query ($isAdult: Boolean, $page: Int, $perPage: Int, $search: String) {
 }
 `;
 
+export function Manga(){
+  const { loading, error, data } = useQuery(GET_DATA,{
+    variables:{
+      isAdult: false,
+      type: "MANGA",
+      perPage: 4
+    }
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.Page.media.map(({ id, title, coverImage, popularity, averageScore, type}) =>(
+    <div>
+        {/* card implementation starts here */}
+            <div  key={id} className="mr-4">
+            <div className="relative bg-white rounded border">
+            <picture className="block bg-gray-200 border-b ">
+                <img className="block object-cover" src={coverImage.large} alt="Image not found" />
+            </picture>
+            <div className="p-4">
+                <h3 className="text-lg font-bold">
+                <a className="stretched-link" href="#" title="Card 1">
+                    {title.romaji}
+                </a>
+                </h3>
+                <p className="block mb-2 text-sm text-gray-600">{averageScore===null?"0":averageScore} average scores</p>
+                <p>
+                    Type: {type}
+                    <br/>
+                    Popularity: {popularity}
+                </p>
+            </div>
+            </div>
+        </div>
+        {/*card implementation ends here*/}    
+    </div>
+));
+}
+
 export default function Anime() {
-    const { loading, error, data } = useQuery(GET_ID,{
+    const { loading, error, data } = useQuery(GET_DATA,{
         variables:{
           isAdult: false,
-          search: searchName===""?null:searchName
+          search: searchName===""?null:searchName,
+          perPage: 4
         }
       });
       if (loading) return <p>Loading...</p>;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { ApolloProvider } from '@apollo/client';
+import { ApolloProvider, useQuery } from '@apollo/client';
 import Navbar, { siteTitle } from '../components/navbar.js'
-import Anime,{client} from './contents.js'
+import Anime,{client, Manga} from './contents.js'
 
 
 export default function Home() {
 
-
-return(
+return (
+  <ApolloProvider client={client}>
     <div>
       <Navbar home />
       <Head>
@@ -16,16 +16,16 @@ return(
         <title>{siteTitle}</title>
       </Head>
 
-      <div className="container mx-auto">
+        <div className="container mx-auto">
         </div>        
         <div className="container mx-auto p-8">
         <div className="flex flex-row flex-wrap -mx-2">
-        <ApolloProvider client={client}>
-          <Anime/>
-        </ApolloProvider>
+        <Anime/>
+        <Manga/>
         </div>
         </div>
     </div>
+  </ApolloProvider>
   );
 }
 
